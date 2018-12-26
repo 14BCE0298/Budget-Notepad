a469322a9ba5bb8ff332650ffd76aadd84e16d14
Vishal Sharma <vishal.k.sharma@oracle.com>
2018-12-26 18:04:27 +0530

    attaching event listener to add item

--- app.js
+++ app.js
@@ -4,8 +4,40 @@ var budgetController = (function() {
 
 var uiController = (function() {
 
+    var domString = {
+        inputBtn:'.add__btn'
+    };
+
+    return {
+        getDOMstrings: function() {
+            return domString;
+        }
+    };
+
 })();
 
-var appController = (function() {
+var appController = (function(budgetCtrl, uiCtrl) {
 
-})();
+    var addItem = function() {
+        console.log('Item added');
+    }
+
+    var setupApp = function() {
+        console.log('App has started');
+        var domItems = uiCtrl.getDOMstrings();
+        document.querySelector(domItems.inputBtn).addEventListener('click', addItem);
+        document.addEventListener('keypress', function(event) {
+            if(event.keyCode === 13 || event.which === 13) {
+                addItem();
+            }
+        });
+    }
+
+    return {
+        init: function() {
+            return setupApp();
+        }
+    }
+})(budgetController, uiController);
+
+appController.init();
